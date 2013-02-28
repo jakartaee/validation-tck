2e87cf706252f9133ada4432e8772c45b54e444a
Gunnar Morling <gunnar.morling@googlemail.com>
2013-02-28 15:19:05 +0100

    BVTCK-42 Marking tests for "validated-executables" option

--- tests/src/main/java/org/hibernate/beanvalidation/tck/tests/bootstrap/BootstrapConfigurationWithValidatedExecutableTypesContainingALLAndNONETest.java
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/bootstrap/BootstrapConfigurationWithValidatedExecutableTypesContainingALLAndNONETest.java
@@ -24,6 +24,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecAssertions;
 import org.jboss.test.audit.annotations.SpecVersion;
 import org.testng.annotations.Test;
 
@@ -50,7 +51,10 @@ public class BootstrapConfigurationWithValidatedExecutableTypesContainingALLAndN
 	}
 
 	@Test
-	@SpecAssertion(section = "5.5.3", id = "b")
+	@SpecAssertions({
+			@SpecAssertion(section = "5.5.3", id = "b"),
+			@SpecAssertion(section = "5.5.6", id = "j")
+	})
 	public void testGetValidatedExecutableTypesReturnsSetWithAllOptionsIfALLAndNONEAreContained() {
 		BootstrapConfiguration bootstrapConfiguration = TestUtil.getConfigurationUnderTest()
 				.getBootstrapConfiguration();
