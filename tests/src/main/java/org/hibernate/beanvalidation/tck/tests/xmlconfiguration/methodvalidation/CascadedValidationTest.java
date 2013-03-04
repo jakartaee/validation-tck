7eec4e5f69c7848cbdd85d0625849d725a0dcdef
Hardy Ferentschik <hardy@ferentschik.de>
2013-03-04 12:36:18 +0100

    BVTCK-42 Method validation test configured via XML

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/xmlconfiguration/methodvalidation/CascadedValidationTest.java
@@ -0,0 +1,74 @@
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual contributors
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.hibernate.beanvalidation.tck.tests.xmlconfiguration.methodvalidation;
+
+import javax.validation.Valid;
+import javax.validation.metadata.MethodDescriptor;
+import javax.validation.metadata.ParameterDescriptor;
+import javax.validation.metadata.ReturnValueDescriptor;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+import org.hibernate.beanvalidation.tck.util.TestUtil;
+import org.hibernate.beanvalidation.tck.util.shrinkwrap.WebArchiveBuilder;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * @author Hardy Ferentschik
+ */
+@SpecVersion(spec = "beanvalidation", version = "1.1.0")
+public class CascadedValidationTest extends Arquillian {
+
+	@Deployment
+	public static WebArchive createTestArchive() {
+		return new WebArchiveBuilder()
+				.withTestClass( CascadedValidationTest.class )
+				.withClass( Cascaded.class )
+				.withValidationXml( "validation-CascadedValidationTest.xml" )
+				.withResource( "CascadedValidationTest.xml" )
+				.build();
+	}
+
+	// used by the test framework
+	public CascadedValidationTest() {
+	}
+
+
+	@Valid
+	CascadedValidationTest(@Valid String s) {
+	}
+
+	@Test
+	@SpecAssertion(section = "8.1.1.5", id = "o")
+	public void testValidaAnnotationIsApplied() throws Exception {
+		MethodDescriptor descriptor = TestUtil.getMethodDescriptor( Cascaded.class, "cascade", String.class );
+		assertNotNull( descriptor, "the specified method should be configured in xml" );
+
+		ReturnValueDescriptor returnValueDescriptor = descriptor.getReturnValueDescriptor();
+		assertTrue( returnValueDescriptor.isCascaded(), "Cascaded validation should be applied" );
+
+		ParameterDescriptor parameterDescriptor = descriptor.getParameterDescriptors().get( 0 );
+		assertTrue( parameterDescriptor.isCascaded(), "Cascaded validation should be applied" );
+	}
+}
---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/xmlconfiguration/methodvalidation/IgnoreAnnotationsOnMethodTest.java
@@ -0,0 +1,83 @@
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual contributors
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.hibernate.beanvalidation.tck.tests.xmlconfiguration.methodvalidation;
+
+import javax.validation.metadata.CrossParameterDescriptor;
+import javax.validation.metadata.MethodDescriptor;
+import javax.validation.metadata.ParameterDescriptor;
+import javax.validation.metadata.ReturnValueDescriptor;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecAssertions;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+import org.hibernate.beanvalidation.tck.util.TestUtil;
+import org.hibernate.beanvalidation.tck.util.shrinkwrap.WebArchiveBuilder;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * @author Hardy Ferentschik
+ */
+@SpecVersion(spec = "beanvalidation", version = "1.1.0")
+public class IgnoreAnnotationsOnMethodTest extends Arquillian {
+	@Deployment
+	public static WebArchive createTestArchive() {
+		return new WebArchiveBuilder()
+				.withTestClass( IgnoreAnnotationsOnMethodTest.class )
+				.withClass( IgnoreAnnotations.class )
+				.withValidationXml( "validation-IgnoreAnnotationsOnMethodTest.xml" )
+				.withResource( "ignore-annotations-IgnoreAnnotationsOnMethodTest.xml" )
+				.build();
+	}
+
+	@Test
+	@SpecAssertions({
+			@SpecAssertion(section = "8.1.1.5", id = "l"),
+			@SpecAssertion(section = "8.1.1.5", id = "m"),
+			@SpecAssertion(section = "8.1.1.5", id = "n"),
+			@SpecAssertion(section = "8.1.1.5", id = "q")
+	})
+	public void testIgnoreAnnotationsOnMethodLevel() {
+		MethodDescriptor descriptor = TestUtil.getMethodDescriptor(
+				IgnoreAnnotations.class,
+				"foobar",
+				String.class,
+				String.class
+		);
+		CrossParameterDescriptor crossParameterDescriptor = descriptor.getCrossParameterDescriptor();
+		assertFalse( crossParameterDescriptor.hasConstraints(), "Cross parameter constraints should be ignored." );
+
+		ReturnValueDescriptor returnValueDescriptor = descriptor.getReturnValueDescriptor();
+		assertFalse( returnValueDescriptor.hasConstraints(), "Return value constraints should be ignored." );
+		assertTrue( returnValueDescriptor.getGroupConversions().isEmpty(), "Group conversions should be ignored" );
+
+		ParameterDescriptor parameterDescriptor = descriptor.getParameterDescriptors().get( 0 );
+		assertFalse( parameterDescriptor.hasConstraints(), "First parameter constraints should be ignored." );
+		assertTrue( parameterDescriptor.getGroupConversions().isEmpty(), "Group conversions should be ignored" );
+
+		parameterDescriptor = descriptor.getParameterDescriptors().get( 1 );
+		assertTrue( parameterDescriptor.hasConstraints(), "Second parameter constraints should be applied." );
+		assertEquals( parameterDescriptor.getGroupConversions().size(), 2, "All group conversions should be combined" );
+	}
+}
