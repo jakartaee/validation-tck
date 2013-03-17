f78cc9e162e19fa55d6313887290a6dcbbf44f85
Gunnar Morling <gunnar.morling@googlemail.com>
2013-03-17 14:02:49 +0100

    BVTCK-48 Adding test for globally disabling executable validation

--- tests/src/main/java/org/hibernate/beanvalidation/tck/tests/integration/cdi/executable/globallydisabled/ExecutableValidationGloballyDisabledTest.java
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/integration/cdi/executable/globallydisabled/ExecutableValidationGloballyDisabledTest.java
@@ -14,11 +14,9 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package org.hibernate.beanvalidation.tck.tests.integration.cdi.executable.global;
+package org.hibernate.beanvalidation.tck.tests.integration.cdi.executable.globallydisabled;
 
 import javax.inject.Inject;
-import javax.validation.ConstraintViolationException;
-import javax.validation.constraints.NotNull;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
@@ -31,16 +29,14 @@ import org.testng.annotations.Test;
 import org.hibernate.beanvalidation.tck.util.IntegrationTest;
 import org.hibernate.beanvalidation.tck.util.shrinkwrap.WebArchiveBuilder;
 
-import static org.hibernate.beanvalidation.tck.util.TestUtil.assertCorrectConstraintTypes;
 import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.fail;
 
 /**
  * @author Gunnar Morling
  */
 @IntegrationTest
 @SpecVersion(spec = "beanvalidation", version = "1.1.0")
-public class ExecutableValidationBasedOnGlobalConfigurationTest extends Arquillian {
+public class ExecutableValidationGloballyDisabledTest extends Arquillian {
 
 	@Inject
 	private CalendarService calendar;
@@ -48,8 +44,8 @@ public class ExecutableValidationBasedOnGlobalConfigurationTest extends Arquilli
 	@Deployment
 	public static WebArchive createTestArchive() {
 		return new WebArchiveBuilder()
-				.withTestClassPackage( ExecutableValidationBasedOnGlobalConfigurationTest.class )
-				.withValidationXml( "validation-ExecutableValidationBasedOnGlobalConfigurationTest.xml" )
+				.withTestClassPackage( ExecutableValidationGloballyDisabledTest.class )
+				.withValidationXml( "validation-ExecutableValidationGloballyDisabledTest.xml" )
 				.withEmptyBeansXml()
 				.build();
 	}
@@ -57,25 +53,13 @@ public class ExecutableValidationBasedOnGlobalConfigurationTest extends Arquilli
 	@Test
 	@SpecAssertions({
 			@SpecAssertion(section = "5.5.6", id = "k"),
-			@SpecAssertion(section = "10.1.2", id = "g")
+			@SpecAssertion(section = "10.1.2", id = "q")
 	})
-	public void testValidationOfConstrainedMethodOnTypeAnnotatedWithValidateOnExecutionContainingExecutableType() {
-		try {
-			calendar.getEvent();
-			fail( "Method invocation should have caused a ConstraintViolationException" );
-		}
-		catch ( ConstraintViolationException e ) {
-			assertCorrectConstraintTypes( e.getConstraintViolations(), NotNull.class );
-		}
-	}
-
-	@Test
-	@SpecAssertion(section = "10.1.2", id = "g")
-	public void testValidationOfConstrainedMethodOnTypeAnnotatedWithValidateOnExecutionNotContainingExecutableType() {
+	public void testExecutableValidationGloballyTurnedOff() {
 		Event event = calendar.createEvent( null );
 		assertNotNull( event );
 
 		// success; the constraint is invalid, but no violation exception is
-		// expected since the executable type is not given in META-INF/validation.xml
+		// expected since executable validation is turned off in META-INF/validation.xml
 	}
 }
