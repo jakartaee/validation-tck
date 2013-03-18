ee66b26997a46e15efd540801908430a803e9176
Gunnar Morling <gunnar.morling@googlemail.com>
2013-03-18 12:14:37 +0100

    BVTCK-48 Adding tests for getter definition

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/validation/GetterDefinitionTest.java
@@ -0,0 +1,76 @@
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual contributors
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.hibernate.beanvalidation.tck.tests.validation;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import org.hibernate.beanvalidation.tck.util.TestUtil;
+import org.hibernate.beanvalidation.tck.util.shrinkwrap.WebArchiveBuilder;
+
+import static org.hibernate.beanvalidation.tck.util.TestUtil.assertCorrectConstraintTypes;
+
+/**
+ * @author Gunnar Morling
+ */
+@SpecVersion(spec = "beanvalidation", version = "1.1.0")
+public class GetterDefinitionTest extends Arquillian {
+
+	private Validator validator;
+
+	@Deployment
+	public static WebArchive createTestArchive() {
+		return new WebArchiveBuilder()
+				.withTestClass( GetterDefinitionTest.class )
+				.withClasses( Shipment.class )
+				.build();
+	}
+
+	@BeforeMethod
+	public void setupValidator() {
+		validator = TestUtil.getValidatorUnderTest();
+	}
+
+	@Test
+	@SpecAssertion(section = "4.1", id = "d")
+	public void testGetterMethod() {
+		Shipment shipment = new Shipment();
+
+		Set<ConstraintViolation<Shipment>> constraintViolations = validator.validateProperty( shipment, "id" );
+		assertCorrectConstraintTypes( constraintViolations, NotNull.class );
+	}
+
+	@Test
+	@SpecAssertion(section = "4.1", id = "d")
+	public void testBooleanGetterMethod() {
+		Shipment shipment = new Shipment();
+
+		Set<ConstraintViolation<Shipment>> constraintViolations = validator.validateProperty( shipment, "shipped" );
+		assertCorrectConstraintTypes( constraintViolations, AssertTrue.class );
+	}
+}
