6d0b257ec07c67607370574d401d3f6f5c915d6a
Guillaume Smet <guillaume.smet@gmail.com>
2017-03-08 10:18:47 +0100

    BVTCK-92 Shorter license header for Java files and license-maven-plugin infrastructure

--- standalone-container-adapter/src/main/java/org/hibernate/validator/tck/arquillian/BeanValidationLocalContainerConfiguration.java
+++ standalone-container-adapter/src/main/java/org/hibernate/validator/tck/arquillian/BeanValidationLocalContainerConfiguration.java
@@ -1,3 +1,9 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
 package org.hibernate.validator.tck.arquillian;
 
 import org.jboss.arquillian.container.spi.ConfigurationException;
