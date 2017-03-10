fd95a3af78bdba389f390e915b90c0f41c657a80
Guillaume Smet <guillaume.smet@gmail.com>
2017-03-10 16:01:47 +0100

    BVTCK-102 Add some more XML versioning tests for BV 2.0

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/xmlconfiguration/versioning/UnknownVersionInValidationXmlTest.java
@@ -0,0 +1,42 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.xmlconfiguration.versioning;
+
+import javax.validation.ValidationException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+import org.hibernate.beanvalidation.tck.util.TestUtil;
+import org.hibernate.beanvalidation.tck.util.shrinkwrap.WebArchiveBuilder;
+
+/**
+ * @author Guillaume Smet
+ */
+@SpecVersion(spec = "beanvalidation", version = "2.0.0")
+public class UnknownVersionInValidationXmlTest extends Arquillian {
+
+	@Deployment
+	public static WebArchive createTestArchive() {
+		return new WebArchiveBuilder()
+				.withTestClass( UnknownVersionInValidationXmlTest.class )
+				.withValidationXml( "validation-UnknownVersionInValidationXmlTest.xml" )
+				.build();
+	}
+
+	@Test(expectedExceptions = ValidationException.class)
+	@SpecAssertion(section = "8.2", id = "c")
+	public void testValidationXmlWithUnknownSchemaVersion() {
+		TestUtil.getConfigurationUnderTest()
+				.buildValidatorFactory()
+				.getValidator();
+	}
+}
---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/xmlconfiguration/versioning/Version20InMappingXmlTest.java
@@ -0,0 +1,50 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.xmlconfiguration.versioning;
+
+import javax.validation.Validator;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+import org.hibernate.beanvalidation.tck.util.TestUtil;
+import org.hibernate.beanvalidation.tck.util.shrinkwrap.WebArchiveBuilder;
+
+import static org.testng.Assert.assertFalse;
+
+/**
+ * @author Guillaume Smet
+ */
+@SpecVersion(spec = "beanvalidation", version = "2.0.0")
+public class Version20InMappingXmlTest extends Arquillian {
+
+	private static final String MAPPING_FILE = "Version20InMappingXmlTest.xml";
+
+	@Deployment
+	public static WebArchive createTestArchive() {
+		return new WebArchiveBuilder()
+				.withTestClass( Version20InMappingXmlTest.class )
+				.withClass( TestEntity.class )
+				.withResource( MAPPING_FILE )
+				.build();
+	}
+
+	@Test
+	@SpecAssertion(section = "8.1.4", id = "a")
+	public void testValidBeanValidation20Mapping() {
+		Validator validator = TestUtil.getConfigurationUnderTest()
+				.addMapping( Version20InMappingXmlTest.class.getResourceAsStream( MAPPING_FILE ) )
+				.buildValidatorFactory()
+				.getValidator();
+
+		assertFalse( validator.getConstraintsForClass( TestEntity.class ).isBeanConstrained() );
+	}
+}
