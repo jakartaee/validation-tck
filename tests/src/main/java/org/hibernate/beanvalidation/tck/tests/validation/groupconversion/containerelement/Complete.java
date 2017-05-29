c430964967623ddaf57ba35c8a797769afa174c3
Guillaume Smet <guillaume.smet@gmail.com>
2017-05-29 14:13:42 +0200

    BVTCK-119 Add tests for XML support of group conversion for container element types

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/validation/groupconversion/containerelement/Complete.java
@@ -0,0 +1,15 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.validation.groupconversion.containerelement;
+
+import javax.validation.groups.Default;
+
+/**
+ * @author Gunnar Morling
+ */
+public interface Complete extends Default, Complex {
+}
---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/validation/groupconversion/containerelement/PostalSequence.java
@@ -0,0 +1,16 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.validation.groupconversion.containerelement;
+
+import javax.validation.GroupSequence;
+
+/**
+ * @author Gunnar Morling
+ */
+@GroupSequence({ BasicPostal.class, ComplexPostal.class })
+public interface PostalSequence {
+}
