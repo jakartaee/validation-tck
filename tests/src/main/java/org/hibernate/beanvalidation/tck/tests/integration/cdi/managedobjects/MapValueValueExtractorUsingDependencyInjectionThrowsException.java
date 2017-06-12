01ea77751bdd484c65d3549ae6a7c433e7c40dcf
Guillaume Smet <guillaume.smet@gmail.com>
2017-06-12 11:52:06 +0200

    BVTCK-153 Add tests for injection of value extractors loaded by the service loader

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/integration/cdi/managedobjects/MapValueValueExtractorUsingDependencyInjectionThrowsException.java
@@ -0,0 +1,28 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.integration.cdi.managedobjects;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.validation.valueextraction.ExtractedValue;
+import javax.validation.valueextraction.ValueExtractor;
+
+/**
+ * @author Guillaume Smet
+ */
+public class MapValueValueExtractorUsingDependencyInjectionThrowsException implements ValueExtractor<Map<?, @ExtractedValue ?>> {
+
+	@SuppressWarnings("unused")
+	@Inject
+	private Greeter greeter;
+
+	@Override
+	public void extractValues(Map<?, ?> originalValue, ValueReceiver receiver) {
+		throw new IllegalStateException( "Shouldn't be called as the XML declared value extractor has precedence." );
+	}
+}
