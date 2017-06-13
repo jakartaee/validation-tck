e3be374ffa5c48802066d9422e5c41c38264ae54
Guillaume Smet <guillaume.smet@gmail.com>
2017-06-13 13:57:33 +0200

    BVTCK-154 Update tck-audit.xml after BVAL-616, BVAL-624 and BVAL-646

--- tests/src/main/java/org/hibernate/beanvalidation/tck/tests/constraints/builtinconstraints/NotBlankConstraintTest.java
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/constraints/builtinconstraints/NotBlankConstraintTest.java
@@ -42,7 +42,7 @@ public class NotBlankConstraintTest extends AbstractTCKTest {
 
 	@Test
 	@SpecAssertion(section = Sections.BUILTINCONSTRAINTS, id = "a")
-	@SpecAssertion(section = Sections.BUILTINCONSTRAINTS, id = "s")
+	@SpecAssertion(section = Sections.BUILTINCONSTRAINTS, id = "u")
 	public void testNotBlankConstraint() {
 		Validator validator = getValidator();
 		NotBlankDummyEntity foo = new NotBlankDummyEntity();
@@ -93,7 +93,7 @@ public class NotBlankConstraintTest extends AbstractTCKTest {
 
 	@Test
 	@SpecAssertion(section = Sections.BUILTINCONSTRAINTS, id = "a")
-	@SpecAssertion(section = Sections.BUILTINCONSTRAINTS, id = "s")
+	@SpecAssertion(section = Sections.BUILTINCONSTRAINTS, id = "u")
 	public void testNotBlankConstraintOnStringBuilder() {
 		// @NotBlank has to support CharSequence so let's also try a StringBuilder
 		Validator validator = getValidator();
