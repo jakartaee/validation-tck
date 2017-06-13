c6586d6a079a86ea2ee27457b51d630788df0f5d
Guillaume Smet <guillaume.smet@gmail.com>
2017-06-13 16:57:22 +0200

    BVTCK-146 Add test for PropertyPathTest and container element information in Section 6.2 - ConstraintViolation

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/validation/ExecutiveProducer.java
@@ -0,0 +1,18 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.validation;
+
+/**
+ * @author Guillaume Smet
+ */
+@ValidExecutiveProducer
+public class ExecutiveProducer extends Employee {
+
+	public ExecutiveProducer(String firstName, String lastName) {
+		super( firstName, lastName );
+	}
+}
