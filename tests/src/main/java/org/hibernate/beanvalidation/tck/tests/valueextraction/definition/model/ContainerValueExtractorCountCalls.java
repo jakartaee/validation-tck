1110bb4b02a0c4c99c8e53fb3f776cd2dbadd8f5
Guillaume Smet <guillaume.smet@gmail.com>
2017-06-15 17:42:46 +0200

    BVTCK-139 Add test to validate that the value extractor is not called for a null container

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/valueextraction/definition/model/ContainerValueExtractorCountCalls.java
@@ -0,0 +1,20 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.valueextraction.definition.model;
+
+import javax.validation.valueextraction.ExtractedValue;
+import javax.validation.valueextraction.ValueExtractor;
+
+public class ContainerValueExtractorCountCalls implements ValueExtractor<Container<@ExtractedValue ?>> {
+
+	public static int callCounter = 0;
+
+	@Override
+	public void extractValues(Container<?> originalValue, ValueReceiver receiver) {
+		callCounter++;
+	}
+}
