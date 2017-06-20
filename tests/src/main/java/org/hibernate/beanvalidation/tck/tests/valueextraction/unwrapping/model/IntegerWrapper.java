6c13e815631198bb7c4f90dabc016517a4674b06
Guillaume Smet <guillaume.smet@gmail.com>
2017-06-20 16:26:58 +0200

    BVTCK-145 BVTCK-149 Add tests for unwrapping configuration and non-generic containers

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/valueextraction/unwrapping/model/IntegerWrapper.java
@@ -0,0 +1,20 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.valueextraction.unwrapping.model;
+
+public class IntegerWrapper {
+
+	private final Integer value;
+
+	public IntegerWrapper(Integer value) {
+		this.value = value;
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+}
---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/valueextraction/unwrapping/model/UnwrapByDefaultIntegerWrapperValueExtractor.java
@@ -0,0 +1,20 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.valueextraction.unwrapping.model;
+
+import javax.validation.valueextraction.ExtractedValue;
+import javax.validation.valueextraction.UnwrapByDefault;
+import javax.validation.valueextraction.ValueExtractor;
+
+@UnwrapByDefault
+public class UnwrapByDefaultIntegerWrapperValueExtractor implements ValueExtractor<@ExtractedValue(type = Integer.class) IntegerWrapper> {
+
+	@Override
+	public void extractValues(IntegerWrapper originalValue, ValueExtractor.ValueReceiver receiver) {
+		receiver.value( null, originalValue.getValue() );
+	}
+}
