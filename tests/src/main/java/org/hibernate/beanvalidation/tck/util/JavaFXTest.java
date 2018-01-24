0eb5d91ec689234202d8698757b5bbf2e91b4c7b
Guillaume Smet <guillaume.smet@gmail.com>
2018-01-24 10:58:58 +0100

    BVTCK-189 Provide a generic way to ignore the JavaFX dependant tests

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/util/JavaFXTest.java
@@ -0,0 +1,23 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.util;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * @author Guillaume Smet
+ */
+@Documented
+@Target({ TYPE })
+@Retention(RUNTIME)
+public @interface JavaFXTest {
+}
