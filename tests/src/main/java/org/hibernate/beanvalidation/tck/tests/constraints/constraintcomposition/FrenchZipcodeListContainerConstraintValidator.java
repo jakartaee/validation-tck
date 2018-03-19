bc32ee00dbce9b4fe9724fbde1ae69d1a0628302
Guillaume Smet <guillaume.smet@gmail.com>
2018-03-19 12:44:39 +0100

    BVTCK-197 Fix mismatch between registered constraints and defined constraints

---
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/constraints/constraintcomposition/FrenchZipcodeListContainerConstraintValidator.java
@@ -0,0 +1,33 @@
+/**
+ * Bean Validation TCK
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.beanvalidation.tck.tests.constraints.constraintcomposition;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.hibernate.beanvalidation.tck.tests.constraints.constraintcomposition.FrenchAddressListContainer.FrenchZipcodeListContainer;
+
+/**
+ * @author Guillaume Smet
+ */
+public class FrenchZipcodeListContainerConstraintValidator implements ConstraintValidator<FrenchZipcodeListContainer, String> {
+
+	@Override
+	public boolean isValid(String zip, ConstraintValidatorContext constraintValidatorContext) {
+		if ( zip == null ) {
+			return true;
+		}
+		if ( "00000".equals( zip ) ) {
+			constraintValidatorContext.disableDefaultConstraintViolation();
+			constraintValidatorContext.buildConstraintViolationWithTemplate( "00000 is a reserved code"  ).addConstraintViolation();
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+}
