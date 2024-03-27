085ed450bf3da772fda31cfd26e445865d38be89
Scott M Stark <starksm64@gmail.com>
2024-03-27 16:56:03 -0600

    Fix the artifact-install.sh script paths
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- setup-examples/artifact-install.sh
+++ setup-examples/artifact-install.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Location where the Validation TCK dist has been extracted
-TCK_DIST=/tmp/BV/beanvalidation-tck-dist-${tck.version}
+TCK_DIST=/tmp/BV/validation-tck-dist-${tck.version}
 
 # Parent pom
 mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file \
 -Dfile=${TCK_DIST}/src/pom.xml -DgroupId=jakarta.validation \
--DartifactId=beanvalidation-tck-parent -Dversion=${tck.version} -Dpackaging=pom
+-DartifactId=validation-tck-parent -Dversion=${tck.version} -Dpackaging=pom
 
 # Install the tests jar
 mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file \
--Dfile=${TCK_DIST}/artifacts/beanvalidation-tck-tests-${tck.version}.jar -DgroupId=jakarta.validation \
--DartifactId=beanvalidation-tck-tests -Dversion=${tck.version} -Dpackaging=jar
+-Dfile=${TCK_DIST}/artifacts/validation-tck-tests-${tck.version}.jar -DgroupId=jakarta.validation \
+-DartifactId=validation-tck-tests -Dversion=${tck.version} -Dpackaging=jar
 
 # Install the test-suite.xml
 mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file \
 -Dfile=${TCK_DIST}/artifacts/tck-tests.xml -DgroupId=jakarta.validation \
--DartifactId=beanvalidation-tck-tests -Dversion=${tck.version} -Dpackaging=xml -Dclassifier=suite
+-DartifactId=validation-tck-tests -Dversion=${tck.version} -Dpackaging=xml -Dclassifier=suite
 
