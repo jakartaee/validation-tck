bec42d28ac779030c334d34e81699803df249675
marko-bekhta <marko.prykladna@gmail.com>
2025-10-09 23:08:14 +0200

    Add tests for version check in the new versions of XSD schemas

--- tests/src/main/java/org/hibernate/beanvalidation/tck/tests/xmlconfiguration/versioning/Version40InValidationXmlTest.java
+++ tests/src/main/java/org/hibernate/beanvalidation/tck/tests/xmlconfiguration/versioning/Version40InValidationXmlTest.java
@@ -8,35 +8,33 @@ package org.hibernate.beanvalidation.tck.tests.xmlconfiguration.versioning;
 
 import static org.testng.Assert.assertEquals;
 
-import jakarta.validation.Configuration;
-
 import org.hibernate.beanvalidation.tck.beanvalidation.Sections;
 import org.hibernate.beanvalidation.tck.tests.AbstractTCKTest;
 import org.hibernate.beanvalidation.tck.util.TestUtil;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.test.audit.annotations.SpecAssertion;
 import org.jboss.test.audit.annotations.SpecVersion;
+
+import jakarta.validation.Configuration;
 import org.testng.annotations.Test;
 
-/**
- * @author Guillaume Smet
- */
-@SpecVersion(spec = "beanvalidation", version = "3.0.0")
-public class Version30InValidationXmlTest extends AbstractTCKTest {
+@SpecVersion(spec = "beanvalidation", version = "4.0.0")
+public class Version40InValidationXmlTest extends AbstractTCKTest {
 
 	@Deployment
 	public static WebArchive createTestArchive() {
 		return webArchiveBuilder()
-				.withTestClass( Version30InValidationXmlTest.class )
+				.withTestClass( Version40InValidationXmlTest.class )
 				.withClass( DummyClockProvider.class )
-				.withValidationXml( "validation-Version30InValidationXmlTest.xml" )
+				.withValidationXml( "validation-Version40InValidationXmlTest.xml" )
 				.build();
 	}
 
 	@Test
 	@SpecAssertion(section = Sections.XML_CONFIG_XSD, id = "a")
-	public void testValidationXmlVersion30() {
+	public void testValidationXmlVersion40() {
 		Configuration<?> config = TestUtil.getConfigurationUnderTest();
 		assertEquals(
 				config.getBootstrapConfiguration().getClockProviderClassName(),
